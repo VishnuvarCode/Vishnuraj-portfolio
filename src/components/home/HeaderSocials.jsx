3224a76da0a035cfd5c257f5e0baaca2e371a964
VishnuvarCode <vishnurajvishnu517@gmail.com>
2024-12-24 14:51:31 +0530

    Updated all files

--- src/components/home/HeaderSocials.jsx
+++ src/components/home/HeaderSocials.jsx
@@ -1,23 +1,45 @@
-import React from 'react';
-import { FaGithub, FaFacebookF, FaLinkedinIn, FaTwitter, FaDev, FaMailBulk } from 'react-icons/fa';
-import { SiHashnode } from 'react-icons/si'
+import React from "react";
+import {
+  FaGithub,
+//   FaFacebookF,
+  FaLinkedinIn,
+//   FaTwitter,
+//   FaDev,
+  FaMailBulk,
+} from "react-icons/fa";
+// import { SiHashnode } from 'react-icons/si'
 
 const HeaderSocials = () => {
-    return (
-        <div className='home__socials'>
-            <a href='https://github.com/VishnuvarCode' className='home__social-link' target='_blank' rel='noreferrer'>
-                <FaGithub />
-            </a>
+  return (
+    <div className="home__socials">
+      <a
+        href="https://github.com/VishnuvarCode"
+        className="home__social-link"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <FaGithub />
+      </a>
 
-            <a href='https://www.linkedin.com/in/v-vishnuraj-2002v' className='home__social-link' target='_blank' rel='noreferrer'>
-                <FaLinkedinIn />
-            </a>
+      <a
+        href="https://www.linkedin.com/in/v-vishnuraj-2002v"
+        className="home__social-link"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <FaLinkedinIn />
+      </a>
 
-            <a href='mailto:vishnuvishnu31221@gmail.com' className='home__social-link' target='_blank' rel='noreferrer'>
-                <FaMailBulk />
-            </a>
-        </div>
-    );
+      <a
+        href="mailto:vishnuvishnu31221@gmail.com"
+        className="home__social-link"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <FaMailBulk />
+      </a>
+    </div>
+  );
 };
 
 export default HeaderSocials;
